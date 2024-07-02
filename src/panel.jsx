79494114f2ad6360ea0c507e129a794ab4938ff1
Taewoong1378 <ktw2378@korea.ac.kr>
2024-07-02 16:57:34 +0900

    FIX : storybook 버전 수정

--- src/panel.jsx
+++ src/panel.jsx
@@ -1,120 +1,129 @@
-import React from 'react';
-import { useParameter, useAddonState } from '@storybook/api';
+import React from "react";
+import { useParameter, useAddonState } from "@storybook/manager-api";
 
-import { PARAM_KEY, ADDON_ID } from './constants';
+import { PARAM_KEY, ADDON_ID } from "./constants";
 
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-import { Form } from '@storybook/components';
+import { Form } from "@storybook/components";
 
-import createParams from './createParams';
+import createParams from "./createParams";
 
 const SourcePanel = () => {
+  let params = getParams();
 
-    let params = getParams();
+  const [fileName, setFileName] = useAddonState(
+    `${ADDON_ID}/fileName`,
+    params.files[0]
+  );
+  const [hideLineNumbers, setHideLineNumbers] = useAddonState(
+    `${ADDON_ID}/hideLineNumbers`,
+    !!params.hideLineNumbers
+  );
 
-    const [fileName, setFileName] = useAddonState(`${ADDON_ID}/fileName`, params.files[0]);
-    const [hideLineNumbers, setHideLineNumbers] = useAddonState(`${ADDON_ID}/hideLineNumbers`, !!params.hideLineNumbers);
+  let fileMap = params.files.reduce(
+    (prev, curr) => ({ ...prev, [curr.fileName]: curr }),
+    {}
+  );
 
-    let fileMap = params.files.reduce((prev, curr) => ({ ...prev, [curr.fileName]: curr }), {});
+  let file = fileMap[fileName] || params.files[0];
 
-    let file = fileMap[fileName] || params.files[0];
-
-
-    return <div>
-        <Form.Field label="Show Line Numbers:">
-            <input
-                type="checkbox"
-                checked={!hideLineNumbers}
-                onChange={(e) => {
-                    setHideLineNumbers(!e.target.checked);
-                }}
-            />
-        </Form.Field>
-        <Form.Field label="File:">
-            <Form.Select
-                value={file.fileName}
-                name='File'
-                onChange={(e) => {
-                    setFileName(e.target.value);
-                }}
-                size='flex'>
-                {params.files.map((x) => (
-                    <option key={x.fileName} value={x.fileName}>
-                        {x.fileName}
-                    </option>
-                ))}
-            </Form.Select>
-        </Form.Field>
-        {
-            file ?
-                <SyntaxHighlighter
-                    key={file.fileName || i}
-                    language={file.language || getLanguage(file.fileName, params)}
-                    showLineNumbers={!hideLineNumbers}
-                    style={params.style || atomDark}>
-                    {file.code.default ? file.code.default : file.code}
-                </SyntaxHighlighter>
-                : null
-        }
-    </div>;
-}
-
+  return (
+    <div>
+      <Form.Field label="Show Line Numbers:">
+        <input
+          type="checkbox"
+          checked={!hideLineNumbers}
+          onChange={(e) => {
+            setHideLineNumbers(!e.target.checked);
+          }}
+        />
+      </Form.Field>
+      <Form.Field label="File:">
+        <Form.Select
+          value={file.fileName}
+          name="File"
+          onChange={(e) => {
+            setFileName(e.target.value);
+          }}
+          size="flex"
+        >
+          {params.files.map((x) => (
+            <option key={x.fileName} value={x.fileName}>
+              {x.fileName}
+            </option>
+          ))}
+        </Form.Select>
+      </Form.Field>
+      {file ? (
+        <SyntaxHighlighter
+          key={file.fileName || i}
+          language={file.language || getLanguage(file.fileName, params)}
+          showLineNumbers={!hideLineNumbers}
+          style={params.style || atomDark}
+        >
+          {file.code.default ? file.code.default : file.code}
+        </SyntaxHighlighter>
+      ) : null}
+    </div>
+  );
+};
 
 function getParams() {
-    const p = useParameter(PARAM_KEY, []);
+  const p = useParameter(PARAM_KEY, []);
 
-    let params = {};
-    if (p.fromContext) {
-
-        params = createParams(p.fromContext);
+  let params = {};
+  if (p.fromContext) {
+    params = createParams(p.fromContext);
+  } else {
+    if (Array.isArray(p)) {
+      params.files = p;
+    } else {
+      if (Array.isArray(p.files)) {
+        params.files = p.files;
+      } else if (p.code) {
+        params.files = [p];
+      } else {
+        params.files = [];
+      }
     }
-    else {
-        if (Array.isArray(p)) {
-            params.files = p;
-        }
-        else {
-            if (Array.isArray(p.files)) {
-                params.files = p.files;
-            }
-            else if (p.code) {
-                params.files = [p];
-            }
-            else {
-                params.files = [];
-            }
-        }
-    }
-    if (p.style) {
-        params.style = p.style;
-    }
+  }
+  if (p.style) {
+    params.style = p.style;
+  }
 
-    params.extensionMapping = p.extensionMapping || {};
+  params.extensionMapping = p.extensionMapping || {};
 
-    if (p.allowedExtensions) {
-        params.files = params.files.filter(x => p.allowedExtensions.indexOf(getExtension(x.fileName)) > -1);
-        params.files = params.files.sort((x, y) => p.allowedExtensions.indexOf(getExtension(x.fileName)) - p.allowedExtensions.indexOf(getExtension(y.fileName)));
-    }
+  if (p.allowedExtensions) {
+    params.files = params.files.filter(
+      (x) => p.allowedExtensions.indexOf(getExtension(x.fileName)) > -1
+    );
+    params.files = params.files.sort(
+      (x, y) =>
+        p.allowedExtensions.indexOf(getExtension(x.fileName)) -
+        p.allowedExtensions.indexOf(getExtension(y.fileName))
+    );
+  }
 
-
-    return params;
+  return params;
 }
 
 function getLanguage(fileName, params) {
-    let extension = getExtension(fileName);
+  let extension = getExtension(fileName);
 
-    const found = params.extensionMapping[extension];
+  const found = params.extensionMapping[extension];
 
-    if (found) { return found; }
-    return extension;
+  if (found) {
+    return found;
+  }
+  return extension;
 }
 
 function getExtension(fileName) {
-    let split = fileName.split('.');
-    let extension = split[split.length - 1];
-    return extension;
+  let split = fileName.split(".");
+  let extension = split[split.length - 1];
+  return extension;
 }
-
 
 export default SourcePanel;
--- src/register.js
+++ src/register.js
@@ -1,32 +1,32 @@
 import React from "react";
 import { AddonPanel } from "@storybook/components";
-import { addons, types } from "@storybook/addons";
-import { useParameter, useAddonState } from '@storybook/api';
+import { addons } from "@storybook/manager-api";
+import { useParameter } from "@storybook/manager-api";
 
-import { ADDON_ID, PARAM_KEY, PANEL_ID } from './constants';
+import { ADDON_ID, PARAM_KEY, PANEL_ID } from "./constants";
 
-import MyPanel from './panel';
+import MyPanel from "./panel";
 
+addons.register(ADDON_ID, (api) => {
+  const render = ({ active, key }) => {
+    const p = useParameter(PARAM_KEY, null);
 
-addons.register(ADDON_ID, api => {
-    
-    const render = ({ active, key }) => {
-        const p = useParameter(PARAM_KEY, null);
+    if (p === null) {
+      return null;
+    }
 
-        if (p === null) {
-            return null;
-        }
+    return (
+      <AddonPanel active={active} key={key}>
+        <MyPanel />
+      </AddonPanel>
+    );
+  };
+  const title = "Code";
 
-        return (<AddonPanel active={active} key={key}>
-            <MyPanel />
-        </AddonPanel>);
-    };
-    const title = 'Code';
-
-    addons.add(PANEL_ID, {
-        type: types.PANEL,
-        title,
-        render,
-        paramKey: PARAM_KEY,
-    });
+  addons.add(PANEL_ID, {
+    type: "panel",
+    title,
+    render,
+    paramKey: PARAM_KEY,
+  });
 });
